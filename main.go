b13920376a2912e1a07b51bcf1786992a3114f38
Matheus Fidelis <matheus@nanoshots.com.br>
2024-01-18 21:44:11 -0300

    first commit

--- main.go
+++ main.go
@@ -17,19 +17,15 @@ func fileSizeInBytes(filePath string) int64 {
 }
 
 func main() {
-	// Verificar se um argumento foi fornecido
+
 	if len(os.Args) < 2 {
-		fmt.Println("Uso: <programa> <caminho_para_imagem>")
 		os.Exit(1)
 	}
 
-	// Pegar o primeiro argumento da linha de comando
 	filePath := os.Args[1]
 
-	// Obter o tamanho do arquivo original
 	originalSize := fileSizeInBytes(filePath)
 
-	// Carregar imagem do disco
 	file, err := os.Open(filePath)
 	if err != nil {
 		panic(err)
@@ -41,7 +37,6 @@ func main() {
 		panic(err)
 	}
 
-	// Redimensionar a imagem
 	resizedImg := resize.Resize(1000, 0, img, resize.Lanczos3)
 
 	out, err := os.Create(filePath)
@@ -50,16 +45,13 @@ func main() {
 	}
 	defer out.Close()
 
-	// Salvar a imagem em formato PNG
 	err = png.Encode(out, resizedImg)
 	if err != nil {
 		panic(err)
 	}
 
-	// Obter o tamanho do arquivo redimensionado
 	resizedSize := fileSizeInBytes(filePath)
 
-	// Imprimir os tamanhos dos arquivos
 	fmt.Printf("Tamanho original: %d bytes\n", originalSize)
 	fmt.Printf("Tamanho após redimensionamento: %d bytes\n", resizedSize)
 }
